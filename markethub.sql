953f4aca798583d404c8a70ac81b6fbe5e55b8e6
tuqa-saeed <tuqasaeed3@gmail.com>
2025-02-21 21:55:27 +0300

    sql db

--- markethub.sql
+++ markethub.sql
@@ -1,31 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Feb 21, 2025 at 07:50 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `markethub`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `auction_items`
---
 
 CREATE TABLE `auction_items` (
   `auction_item_id` int(11) NOT NULL,
